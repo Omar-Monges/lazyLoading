a2809b4590eba4f36b9dc5e4af552726d51702b3
Omar-Monges <devomarmonges@gmail.com>
2021-11-25 20:23:50 -0300

    Loading animation

--- src/lazy.js
+++ src/lazy.js
@@ -1,3 +1,8 @@
+// function removeAnimations(nodo){
+//   for (let i = 0; i < 3; i++) {
+//     nodo.removeChild(nodo.firstChild)
+//   }
+// }
 
 const observer = new IntersectionObserver((entries) => {
   const nodo = entries[0]
@@ -6,6 +11,9 @@ const observer = new IntersectionObserver((entries) => {
     //load image
     img.src = img.dataset.src
     img.onload = () => {
+      console.log('Tenemos q borrar')
+      // removeAnimations(img.parentElement)
+      img.previousElementSibling.remove()
       img.parentElement.className = ''
     }
     // container.classList.remove('wrapper')
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,18 @@
+function nodeAnimation(){
+  const container = document.createElement('div')
+    container.classList.add('lds-facebook')
+  for(let i = 0; i < 3; i++){
+    container.append(document.createElement('div'))
+  }
+  return container
+}
 export const createImageNode = (source) => {
   const wrapper = document.createElement('div'),
+    animations = nodeAnimation(),
     image = document.createElement('img')
-  wrapper.className = 'lds-hourglass'
+  animations.className = 'lds-facebook'
   image.classList.add('addImg')
   image.dataset.src = source
-  wrapper.append(image)
+  wrapper.append(animations, image)
   return [wrapper, image]
 }
